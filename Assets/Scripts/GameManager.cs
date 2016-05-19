37989faf0c7f0f14946b292c8d189dcdba9a85dd
Frezc <504021398@qq.com>
2016-05-20 01:35:15 +0800

    init

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+    // 0 empty -1 road
+    public int[,] gameBoard = new int[27,16];
+
+    // Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

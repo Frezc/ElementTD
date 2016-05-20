afe2981b86f406841f296fa359ec8018ccc31a27
jnereus <281804140@qq.com>
2016-05-20 17:04:00 +0800

    添加了开始界面，完成了塔基的选择提示，将路设为了-1

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,7 +7,53 @@ public class GameManager : MonoBehaviour {
 
     // Use this for initialization
 	void Start () {
-
+        for (int i = 0; i <= 3;i++ )
+        {
+            gameBoard[i, 13] = -1;
+        }
+        for (int i = 2; i <= 13; i++)
+        {
+            gameBoard[4, i] = -1;
+        }
+        for (int i = 4; i <= 8; i++)
+        {
+            gameBoard[i, 2] = -1;
+        }
+        for (int i = 2; i <= 11; i++)
+        {
+            gameBoard[8, i] = -1;
+        }
+        for (int i = 8; i <= 11; i++)
+        {
+            gameBoard[i, 11] = -1;
+        }
+        for (int i = 5; i <= 11; i++)
+        {
+            gameBoard[11, i] = -1;
+        }
+        for (int i = 11; i <= 14; i++)
+        {
+            gameBoard[i, 5] = -1;
+        }
+        for (int i = 5; i <= 8; i++)
+        {
+            gameBoard[14, i] = -1;
+        }
+        for (int i = 14; i <= 18; i++)
+        {
+            gameBoard[i, 8] = -1;
+        }
+        for (int i = 3; i <= 13; i++)
+        {
+            gameBoard[18, i] = -1;
+            gameBoard[25, i] = -1;
+        }
+        for (int i = 18; i <= 25; i++)
+        {
+            gameBoard[i, 3] = -1;
+            gameBoard[i, 13] = -1;
+        }
+        gameBoard[26, 8] = -1;
 	}
 	
 	// Update is called once per frame
---
+++ Assets/Scripts/startButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class startButton : MonoBehaviour {
+    public void MENU_ACTION_GotoPage(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
